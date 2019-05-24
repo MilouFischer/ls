f37599e8cdc804a684b7e78fc74ef79a36cb1140
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-05-24 13:54:15 +0200

    flags errors OK

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,35 +6,51 @@
 /*   By: efischer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 17:25:57 by efischer          #+#    #+#             */
-/*   Updated: 2019/05/24 12:47:23 by efischer         ###   ########.fr       */
+/*   Updated: 2019/05/24 13:52:27 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static uint8_t	ft_manage_args(int *current_arg, int ac, char **av)
+static void		ft_get_flags(char *arg, uint8_t *flags)
 {
 	size_t	i;
+
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		if (arg[i] == 'l')
+			*flags |= FLAG_L;
+		else if (arg[i] == 'a')
+			*flags |= FLAG_A;
+		else if (arg[i] == 'r')
+			*flags |= FLAG_REV;
+		else if (arg[i] == 't')
+			*flags |= FLAG_T;
+		else if (arg[i] == 'R')
+			*flags |= FLAG_R;
+		else
+		{
+			ft_putendl("ft_ls: illegal option -- -\nusage: ft_ls [-Ralrt] [file ...]");
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+}
+
+static uint8_t	ft_manage_args(int *current_arg, int ac, char **av)
+{
 	uint8_t	flags;
 
 	flags = 0;
-	while (*current_arg < ac && av[*current_arg][0] == '-')
+	while (*current_arg < ac && av[*current_arg][0] == '-' && av[*current_arg][1] != '\0')
 	{
-		i = 0;
-		while (av[*current_arg][i] != '\0')
+		if (av[*current_arg][1] == '-')
 		{
-			if (av[*current_arg][i] == 'l')
-				flags |= FLAG_L;
-			else if (av[*current_arg][i] == 'a')
-				flags |= FLAG_A;
-			else if (av[*current_arg][i] == 'r')
-				flags |= FLAG_REV;
-			else if (av[*current_arg][i] == 't')
-				flags |= FLAG_T;
-			else if (av[*current_arg][i] == 'R')
-				flags |= FLAG_R;
-			i++;
+			(*current_arg)++;
+			return (flags);
 		}
+		ft_get_flags(av[*current_arg] + 1, &flags);
 		(*current_arg)++;
 	}
 	return (flags);
