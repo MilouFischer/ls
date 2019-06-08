c25d8af8172fd4a0c13bcdd0fc55efaa7577e35e
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-06-08 11:55:36 +0200

    norm flags.c OK

--- srcs/flags.c
+++ srcs/flags.c
@@ -6,33 +6,40 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:29:10 by efischer          #+#    #+#             */
-/*   Updated: 2019/06/07 16:31:30 by efischer         ###   ########.fr       */
+/*   Updated: 2019/06/08 11:53:38 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "flags.h"
 
+static uint16_t	ft_check_flag(char c, uint16_t flags)
+{
+	if (c == 'l')
+		return (flags |= FLAG_L);
+	else if (c == 'a')
+		return (flags |= FLAG_A);
+	else if (c == 'r')
+		return (flags |= FLAG_REV);
+	else if (c == 'R')
+		return (flags |= FLAG_R);
+	else if (c == 't')
+		return (flags |= FLAG_T);
+	else if (c == 'G')
+		return (flags |= FLAG_G);
+	else if (c == 'd')
+		return (flags |= FLAG_D);
+	else if (c == '1')
+		return (flags |= FLAG_1);
+	else
+		return (FALSE);
+}
+
 static void		ft_get_flags(char *arg, uint16_t *flags)
 {
 	while (*arg != '\0')
 	{
-		if (*arg == 'l')
-			*flags |= FLAG_L;
-		else if (*arg == 'a')
-			*flags |= FLAG_A;
-		else if (*arg == 'r')
-			*flags |= FLAG_REV;
-		else if (*arg == 'R')
-			*flags |= FLAG_R;
-		else if (*arg == 't')
-			*flags |= FLAG_T;
-		else if (*arg == 'G')
-			*flags |= FLAG_G;
-		else if (*arg == 'd')
-			*flags |= FLAG_D;
-		else if (*arg == '1')
-			*flags |= FLAG_1;
-		else
+		*flags = ft_check_flag(*arg, *flags);
+		if (*flags == FALSE)
 		{
 			ft_printf("ft_ls: illegal option -- %c\n", *arg);
 			ft_putendl("usage: ft_ls [-GRadlrt1] [file ...]");
